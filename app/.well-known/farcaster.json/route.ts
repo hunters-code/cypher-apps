f0af0ede15492f461eef47fff2567fcba98f8a24
Nashir Jamali <nashirjamali@gmail.com>
2026-01-31 20:37:03 +0700

    fix: update farcaster

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,36 +4,37 @@ export async function GET() {
 
   const manifest = {
     accountAssociation: {
-      header:
-        "eyJmaWQiOi0xLCJ0eXBlIjoiYXV0aCIsImtleSI6IjB4MjI0MTYyNTM0NjEwNjI2ZEFjYTg0YWQyNjBBMzMxNDg1MTY4QzM2QSJ9",
-      payload: "eyJkb21haW4iOiJjeXBoZXItYXBwcy52ZXJjZWwuYXBwIn0",
-      signature:
-        "AAAAAAAAAAAAAAAAyhG94Fl3s2MRZwKIYr4qFzl2yhEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAiSCrVbLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAul7REO_bo9AFv8iC11NYrLu4WEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASQ_-6NvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAi6b4Q-1CJyfp7uFksI9trTmaWB4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAAPhSELIcxQMC9He6VmhtIBncm2etAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBguudMuesCuIjNSEihiD3MgCZulWTp_BWoXpWzWQhNQRJ1PRAAOtInem7VjpdM1aNJYhcc0xV6J9snVPSdqUFXRwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAZJJkkmSSZJJkkmSSZJJkkmSSZJJkkmSSZJJkkmSSZJI",
+      header: "",
+      payload: "",
+      signature: "",
     },
     miniapp: {
       version: "1",
       name: "Cypher Wallet",
       homeUrl: appUrl,
-      iconUrl: `${appUrl}/embed-image.png`,
-      splashImageUrl: `${appUrl}/splash-image.png`,
+      iconUrl:
+        "https://raw.githubusercontent.com/hunters-code/cypher-apps/refs/heads/main/previews/Cypher-Logo.jpg",
+      splashImageUrl:
+        "https://raw.githubusercontent.com/hunters-code/cypher-apps/refs/heads/main/previews/Cypher-Logo.jpg",
       splashBackgroundColor: "#000000",
       webhookUrl: `${appUrl}/api/webhook`,
       subtitle: "Private crypto wallet",
       description:
         "Your crypto, truly private. Enjoy cash-level privacy on the blockchain.",
       screenshotUrls: [
-        `${appUrl}/s1.png`,
-        `${appUrl}/s2.png`,
-        `${appUrl}/s3.png`,
+        "https://raw.githubusercontent.com/hunters-code/cypher-apps/refs/heads/main/previews/Cypher-Preview-1.jpg",
+        "https://raw.githubusercontent.com/hunters-code/cypher-apps/refs/heads/main/previews/Cypher-Preview-2.jpg",
       ],
       primaryCategory: "finance",
       tags: ["crypto", "wallet", "privacy", "baseapp"],
-      heroImageUrl: `${appUrl}/embed-image.png`,
+      heroImageUrl:
+        "https://raw.githubusercontent.com/hunters-code/cypher-apps/refs/heads/main/previews/Cypher-Hero-Image.jpg",
       tagline: "Your crypto, truly private",
       ogTitle: "Cypher Wallet",
       ogDescription:
         "Your crypto, truly private. Enjoy cash-level privacy on the blockchain.",
-      ogImageUrl: `${appUrl}/embed-image.png`,
+      ogImageUrl:
+        "https://raw.githubusercontent.com/hunters-code/cypher-apps/refs/heads/main/previews/Cypher-Hero-Image.jpg",
       noindex: true,
     },
   };
